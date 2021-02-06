471f33bc8e395fc2694360d1b44395cec5d15346
Mark <18813146635@139.com>
2021-02-06 20:51:36 +0800

    add the *.h

--- BTree/BTNode.h
+++ BTree/BTNode.h
@@ -2,15 +2,15 @@
 
 #pragma once
 
-#include "vector"
+#include "vector/vector.h"
 #define BTNodePosi(T) BTNode<T>* //B-树节点位置
-using namespace std;
+
 
 template <typename T> struct BTNode { //B-树节点模板类
 // 成员（为简化描述起见统一开放，读者可根据需要进一步封装）
    BTNodePosi(T) parent; //父节点
-   vector<T> key; //关键码向量
-   vector<BTNodePosi(T)> child; //孩子向量（其长度总比key多一）
+   Vector<T> key; //关键码向量
+   Vector<BTNodePosi(T)> child; //孩子向量（其长度总比key多一）
 // 构造函数（注意：BTNode只能作为根节点创建，而且初始时有0个关键码和1个空孩子指针）
    BTNode() { parent = NULL; child.insert ( 0, NULL ); }
    BTNode ( T e, BTNodePosi(T) lc = NULL, BTNodePosi(T) rc = NULL ) {
--- BTree/BTree_insert.h
+++ BTree/BTree_insert.h
@@ -4,7 +4,7 @@
 
 template <typename T> bool BTree<T>::insert ( const T& e ) { //将关键码e插入B树中
    BTNodePosi(T) v = search ( e ); if ( v ) return false; //确认目标节点不存在
-   rank r = _hot->key.search ( e ); //在节点_hot的有序关键码向量中查找合适的插入位置
+   Rank r = _hot->key.search ( e ); //在节点_hot的有序关键码向量中查找合适的插入位置
    _hot->key.insert ( r + 1, e ); //将新关键码插至对应的位置
    _hot->child.insert ( r + 2, NULL ); //创建一个空子树指针
    _size++; //更新全树规模
